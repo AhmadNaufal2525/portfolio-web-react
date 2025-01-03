dea8253243d1d5d22da318fda2ef59d73aa513ed
Noorey Schaultz <ahmadnaufal588@gmail.com>
2025-01-03 21:29:13 +0700

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,11 @@ import AboutRoute from "./routes/aboutMeRoute";
 import PortfolioRoute from "./routes/portfolioRoute";
 import ContactRoute from "./routes/contactRoute";
 import NotFound from "./routes/notfoundRoute";
-import Navbar from "./components/navbar";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const location = useLocation();
-
-  // List of valid routes
-  const validRoutes = ["/", "/about", "/portfolio", "/contact"];
-
-  // Check if the current path is valid
-  const isValidRoute = validRoutes.includes(location.pathname);
-
   return (
-    <>
-      {/* Conditionally render Navbar only for valid routes */}
-      {isValidRoute && <Navbar />}
+    <Router>
       <Routes>
         <Route path="/" element={<HomeRoute />} />
         <Route path="/about" element={<AboutRoute />} />
@@ -32,16 +16,8 @@ function App() {
         <Route path="/contact" element={<ContactRoute />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
-  );
-}
-
-function Root() {
-  return (
-    <Router>
-      <App />
     </Router>
   );
 }
 
-export default Root;
+export default App;
