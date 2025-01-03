7cc0ae0cc03be35d238b346b5533dbdca2598255
Noorey Schaultz <ahmadnaufal588@gmail.com>
2025-01-03 20:52:36 +0700

    update main.jsx and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
 
 function Root() {
   return (
-    <Router>
+    <Router basename="/portfolio-web-react/">
       <App />
     </Router>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio-web-react/">
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
