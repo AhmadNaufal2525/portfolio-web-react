35252916c2560527a92dba225c372202353ef979
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2026-05-12 14:27:52 +0700

    remove some project

--- data/projectsData.jsx
+++ data/projectsData.jsx
@@ -1,7 +1,5 @@
 import KulinerGo from "../src/assets/portfolio/kuliner-go.png";
 import Adma from "../src/assets/portfolio/adma-mob.png";
-import Antria from "../src/assets/portfolio/antria.png";
-import Tactin from "../src/assets/portfolio/tactin.png";
 import Portfolio from "../src/assets/portfolio/portfolio.png";
 import AdmaScreen from "../src/assets/portfolio/adma-screen.png";
 import QA from "../src/assets/portfolio/qa.png";
@@ -32,35 +30,6 @@ export const projects = [
   },
   {
     id: 3,
-    title: "Antria App",
-    description: "Mobile app for managing appointments and bookings.",
-    tags: [
-      "Flutter",
-      "Mobile App",
-      "Clean Architecture",
-      "API Integration",
-      "BLoC",
-    ],
-    detailedDescription:
-      "Antria application is a website and mobile app platform that provides convenience for users (customers and partners). The Antria application allows customers to place orders much faster because they can perform a Virtual Queue to make an order, and for partners, they can see customer orders based on the queue number of customers who place orders through the Virtual Queue.",
-    category: "Mobile Apps",
-    image: Antria,
-    link: "https://github.com/Antria-id/antria-mitra-mobile.git",
-  },
-  {
-    id: 4,
-    title: "Tactin App",
-    description:
-      "Mobile application designed to translate documents and text into accessible formats for visually impaired users.",
-    detailedDescription:
-      "Tactin is a mobile application designed to translate documents and text into accessible formats for visually impaired users. The app can convert text into Braille, allowing users to read and interact with content through tactile feedback and also can translate file formats such as PDF into Braille.",
-    tags: ["Flutter", "Dart", "Mobile App"],
-    category: "Mobile Apps",
-    image: Tactin,
-    link: "https://github.com/AhmadNaufal2525/braille-app.git",
-  },
-  {
-    id: 5,
     title: "Portfolio Website",
     description: "Personal portfolio website showcasing projects and skills.",
     detailedDescription:
@@ -71,7 +40,7 @@ export const projects = [
     link: "https://ahmadnaufaldp.vercel.app/",
   },
   {
-    id: 6,
+    id: 4,
     title: "QA Project Capstone",
     description:
       "A comprehensive software quality assurance project for a web application.",
